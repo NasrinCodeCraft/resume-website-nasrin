56b15ac0b812edc57baebaacee065da7ba9fa322
NasrinCodeCraft <mokhtarinasrin200@gmail.com>
2023-11-18 09:06:27 -0800

    add my profile image in home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,6 @@
 import prorfile from './../../assets/images/profile.png';
+import wave from './../../assets/images/wave.svg';
+import trees from './../../assets/images/Trees.svg';
 import styled from "./home.module.css";
 
 function Home() {
@@ -6,8 +8,9 @@ function Home() {
     return (
         <div className={`container ${styled.backgroundColorSky}`}>
             <div className={styled.stars}></div>
-            {/* <img className={styled.profile} src={prorfile} /> */}
+            <img className={styled.profile} src={prorfile} />
             <div className={styled.moon}></div>
+            <img src={wave} className={styled.wave}/>
         </div>
     )
 

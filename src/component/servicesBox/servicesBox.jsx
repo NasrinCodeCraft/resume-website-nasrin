c23f8358e92272b6dbdfc810e7bbdeaaa796dc5a
NasrinCodeCraft <mokhtarinasrin200@gmail.com>
2023-12-01 01:46:20 -0800

    services page with axios and props

--- src/component/servicesBox/servicesBox.jsx
+++ src/component/servicesBox/servicesBox.jsx
@@ -1,25 +1,15 @@
 import styled from './servicesBox.module.css';
-import webIcon from './../../assets/images/web.svg';
 
-
-
-function ServicesBox() {
+function ServicesBox(props) {
 
     return (
         <div className={styled.box}>
-            <img src={webIcon} className={styled.iconService}/>
-            <p className={styled.titleService}>title service</p>
-            <p className={styled.contentService}>
-                Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Eius esse accusantium, 
-                tenetur expedita incidunt iste eligendi, 
-                corporis modi consequatur ipsa eos. 
-                Reiciendis dolore pariatur laborum 
-                veritatis animi necessitatibus ad cupiditate?
-            </p>
+        <img src={props.box.icon} className={styled.iconService} alt="Service Icon" />
+            <p className={styled.titleService}>{props.box.title}</p>
+            <p className={styled.contentService}>{props.box.Desc}</p>
         </div>
     )
-    
+
 }
 
 export default ServicesBox;
--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import ServicesBox from '../../component/servicesBox/servicesBox';
 import styled from './services.module.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function Services() {
 
+    const [boxes , setBoxes] = useState([]);
+
+    useEffect (()=>{
+
+        axios
+        .get("http://localhost:5000/boxes")
+        // create top link with bottom terminal
+        //PS F:\my-site> npx json-server --watch data/db.json --port 5000
+        .then((result)=>{
+            setBoxes(result.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+
+    },[]);
+    
     return (
         <>
             <div className={styled.titleService}>
@@ -10,10 +30,11 @@ function Services() {
                 <p>What I do</p>
             </div>
             <div className={styled.servicesBox}>
-                <ServicesBox />
-                <ServicesBox />
-                <ServicesBox />
-                {/* component servicebox with props */}
+                {
+                    boxes.map(result => (
+                        <ServicesBox box={result} key={result.id} />
+                    ))  
+                }
             </div>
         </>
     )

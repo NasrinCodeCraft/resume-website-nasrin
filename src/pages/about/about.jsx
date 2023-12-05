d003950604b3aec461747dddbe96446cf7e56885
NasrinCodeCraft <mokhtarinasrin200@gmail.com>
2023-12-05 09:32:21 -0800

    New feature for navbar & about us

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,22 +1,28 @@
 import styled from './about.module.css';
 import BlueStars from '../../component/navbar/blueStars/blueStars';
+import greenprofile from './../../assets/images/greenprofile.png'
 
 function AboutUs() {
 
     return (
 
-        <div className={`${styled.backgroundColorSky}`}>
+        <div className={`container ${styled.backgroundColorSky}`}>
             <div className={styled.bio}>
                 <pre>
-                    <p>Who's this girl?</p>
-                    <p>My name is Nasrin.</p>
-                    I am a web designer and developer living in beautiful Tehran, Iran.<br/> 
-                    I enjoy solving problems and making everything beautiful and easy.<br/>
-                    You can use my skills. I can't stop learning new things.<br/> 
-                    the more the better. I am also very interested in stars and galaxies.<br/> 
-                    I like it. My favorite hobby is bodybuilding and sports in general.<br/>
-                    And I love to eat kaffee.
+                    <p><span>Who's</span> this girl?<br/>
+                    My name is <span>Nasrin</span>.</p>
+                    I am a web designer and developer living in<br/> 
+                    beautiful Tehran, Iran. I enjoy solving<br/> 
+                    problems and making everything beautiful<br/>
+                    and easy.You can use my skills. I can't stop<br/>
+                    learning new things. the more the better.<br/>
+                    I am also very interested in stars and galaxies.<br/> 
+                    I like it. My favorite hobby is bodybuilding <br/>
+                    and sports in general. And I love to eat kaffee.
                 </pre>
+            </div>
+            <div className={styled.box_porofile}>
+                <img src={greenprofile} />
             </div>
             <BlueStars />
         </div>

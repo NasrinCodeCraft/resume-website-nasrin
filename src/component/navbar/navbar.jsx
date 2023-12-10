ab08ae23457848270e90153e873c892453eab275
NasrinCodeCraft <mokhtarinasrin200@gmail.com>
2023-12-10 00:04:43 -0800

    hamburger menu

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,24 +1,37 @@
 import styled from "./navbar.module.css";
 import { Link, Element, scroller } from "react-scroll";
+import React, { useState } from "react";
 
 function Navbar() {
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+
     return (
         <div className={styled.header}>
-            <ul>
-                <li>
-                    <Link to="home" smooth={true} duration={500}>Home</Link>
-                </li>
-                <li>
-                    <Link to="about" smooth={true} duration={500}>About me</Link>
-                </li>
-                <li>
-                    <Link to="services" smooth={true} duration={500}>Services</Link>
-                </li>
-                <li>
-                    <Link to="contact" smooth={true} duration={500}>Contact me</Link>
-                </li>
-            </ul>
+                <label className={styled.menuToggle} htmlFor="demo" onClick={toggleMenu}>
+                    <input type="checkbox" id="demo" checked={menuOpen} onChange={toggleMenu}/>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+                <ul style={{ left: menuOpen ? "0px" : "-100%" }}>
+                    <li>
+                        <Link to="home" smooth={true} duration={500}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="about" smooth={true} duration={500}>About me</Link>
+                    </li>
+                    <li>
+                        <Link to="services" smooth={true} duration={500}>Services</Link>
+                    </li>
+                    <li>
+                        <Link to="contact" smooth={true} duration={500}>Contact me</Link>
+                    </li>
+                </ul>
         </div>
     )
 

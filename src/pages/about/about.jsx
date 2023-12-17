1b7db8bf6412e63608e0e11f51e119e4fdbf9fa6
NasrinCodeCraft <mokhtarinasrin200@gmail.com>
2023-12-17 05:59:36 -0800

    responsive about us page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -7,22 +7,25 @@ function AboutUs() {
     return (
 
         <div className={`container ${styled.backgroundColorSky}`}>
-            <div className={styled.bio}>
-                <pre>
-                    <p><span>Who's</span> this girl?<br/>
-                    My name is <span>Nasrin</span>.</p>
-                    I am a web designer and developer living in<br/> 
-                    beautiful Tehran, Iran. I enjoy solving<br/> 
-                    problems and making everything beautiful<br/>
-                    and easy.You can use my skills. I can't stop<br/>
-                    learning new things. the more the better.<br/>
-                    I am also very interested in stars and galaxies.<br/> 
-                    I like it. My favorite hobby is bodybuilding <br/>
-                    and sports in general. And I love to eat kaffee.
-                </pre>
-            </div>
-            <div className={styled.box_porofile}>
-                <img src={greenprofile} />
+            <p className={styled.who}><span>Who's</span> this girl?<br/>
+                    My name is <span>Nasrin</span>.
+            </p>
+            <div className={styled.orderingAboutContent}>
+                <div className={styled.bio}>
+                    <p>
+                        I am a web designer and developer living in
+                        beautiful Tehran, Iran. I enjoy solving
+                        problems and making everything beautiful
+                        and easy.You can use my skills. I can't stop
+                        learning new things. the more the better.
+                        I am also very interested in stars and galaxies.
+                        I like it. My favorite hobby is bodybuilding 
+                        and sports in general. And I love to eat kaffee.
+                    </p>
+                </div>
+                <div className={styled.box_porofile}>
+                    <img src={greenprofile} />
+                </div>
             </div>
             <BlueStars />
         </div>
